b980be9b9f3fa81822f8cbe4a43958f0b262880a
EduFer1987 <EduFer1987@github.com>
2026-03-07 11:07:10 +0100

    Assignment 4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -7,27 +7,73 @@
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+unsigned int function_calls;
 
 void* threadfunc(void* thread_param)
 {
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    thread_func_args->thread_complete_success = false;
+
+    usleep((unsigned int) ((thread_func_args->wait_to_obtain_ms)*1000));
+    thread_func_args->mutex_lock_rtrn_vle = pthread_mutex_lock(thread_func_args->mutex_info);
+    if (thread_func_args->mutex_lock_rtrn_vle != 0)
+    {
+      return thread_func_args;
+    }
+
+    usleep((unsigned int) ((thread_func_args->wait_to_release_ms)*1000));
+    thread_func_args->mutex_lock_rtrn_vle = pthread_mutex_unlock(thread_func_args->mutex_info);
+    printf("After unlock\n");
+    if (thread_func_args->mutex_lock_rtrn_vle != 0)
+    {
+      return thread_func_args;
+    }
+    
+    thread_func_args->thread_complete_success = true;
+    return thread_func_args;
+
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
     /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
+     * TODO: allocate memory for thread_data, setup mutex and wait arguments
+    */
+    struct thread_data *thread_data_inst               = calloc(1, sizeof(struct thread_data));
+    printf("'%d'\n", function_calls);   
+    printf("wait_to_obtain_ms: %d\n", wait_to_obtain_ms); 
+    printf("wait_to_release_ms: %d\n", wait_to_release_ms);
+    if (NULL == thread_data_inst)
+    {
+      return false;
+    }
+    thread_data_inst[0].mutex_info              = mutex;
+    thread_data_inst[0].thread_complete_success = false;
+    thread_data_inst[0].wait_to_obtain_ms       = wait_to_obtain_ms;
+    thread_data_inst[0].wait_to_release_ms      = wait_to_release_ms;
+     /**
+     * TODO: pass thread_data to created thread
      * using threadfunc() as entry point.
      *
      * return true if successful.
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    if ( pthread_create(thread, NULL, threadfunc, thread_data_inst) != 0)
+    {
+      printf("If\n");
+      free(thread_data_inst);
+      return false;
+    }
+    else
+    {
+      printf("Else\n");
+      thread_data_inst[0].thread = thread;
+      return true;
+    }
 }
 
